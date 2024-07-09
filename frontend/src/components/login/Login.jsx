705b11f63569150c5f000be1a3453e3baec0c793
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2024-07-10 01:17:22 +0530

    added routes and components

---
+++ frontend/src/components/login/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Login(){
+    return(
+        <div className='text-black'>This is Login Page</div>
+    )
+} 
+
+export default Login;
---
+++ frontend/src/components/signup/Signup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Signup(){
+    return(
+        <div>This is Signup Page</div>
+    )
+} 
+
+export default Signup;
