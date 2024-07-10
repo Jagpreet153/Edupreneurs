ce4d074ab79daf47f2f2e228590af5ab8eddfab2
Biraj Sanghai <sanghaibiraj@gmail.com>
2024-07-10 11:10:29 +0530

    added more components

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+function Home(){
+    return(
+        <div className='text-black'>This is Home Page</div>
+    )
+} 
+
+export default Home;
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+function Navbar(){
+    return(
+        <div className='text-black'>This is the navbar</div>
+    )
+} 
+
+export default Navbar;
