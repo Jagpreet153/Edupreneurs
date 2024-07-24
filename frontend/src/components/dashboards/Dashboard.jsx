bdbc24fddd1435006cd6d01cac0a8c34bec55c1d
Biraj Sanghai <sanghaibiraj@gmail.com>
2024-07-24 15:28:43 +0530

    added frontend of edit profile

--- frontend/src/components/dashboards/Dashboard.jsx
+++ frontend/src/components/dashboards/Dashboard.jsx
@@ -28,34 +28,32 @@ export const cards = [
   },
 ];
 
-
-
 function Dashboard() {
   return (
     <div>
       <Chatbot />
       <div className="p-8">
-          <div
-            className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full gap-4`}
-          >
-            {cards.map((card, index) => (
-              <div
-                key={index}
-                className="card flex-auto z-0 bg-secondary text-neutral-content"
-              >
-                <div className="card-body items-center text-center">
-                  <h2 className="card-title"> {card.className} </h2>
-                  <p>{card.dateStart}</p>
-                  <p>{card.ownerName}</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Open</button>
-                  </div>
+        <div
+          className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full gap-4`}
+        >
+          {cards.map((card, index) => (
+            <div
+              key={index}
+              className="card flex-auto z-0 bg-secondary text-neutral-content"
+            >
+              <div className="card-body items-center text-center">
+                <h2 className="card-title"> {card.className} </h2>
+                <p>{card.dateStart}</p>
+                <p>{card.ownerName}</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Open</button>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
+    </div>
   );
 }
 
